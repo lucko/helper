a50743926021f56b530ff978c1a1e3dad7a395af
Luck <git@lucko.me>
2017-07-25 12:38:22 +0100

    Implement mavenlibrary util, update readme

---
+++ helper/src/main/java/me/lucko/helper/maven/LibraryLoader.java
@@ -0,0 +1,205 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.maven;
+
+import com.google.common.base.Preconditions;
+
+import me.lucko.helper.utils.LoaderUtils;
+import me.lucko.helper.utils.Log;
+
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+
+/**
+ * Resolves {@link MavenLibrary} annotations for a class, and loads the dependency
+ * into the classloader.
+ */
+public final class LibraryLoader {
+    private static Method ADD_URL_METHOD;
+    static {
+        try {
+            ADD_URL_METHOD = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            ADD_URL_METHOD.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Resolves all {@link MavenLibrary} annotations on the given object.
+     *
+     * @param object the object to load libraries for.
+     */
+    public static void loadAll(Object object) {
+        loadAll(object.getClass());
+    }
+
+    /**
+     * Resolves all {@link MavenLibrary} annotations on the given class.
+     *
+     * @param clazz the class to load libraries for.
+     */
+    public static void loadAll(Class<?> clazz) {
+        MavenLibraries libs = clazz.getAnnotation(MavenLibraries.class);
+        if (libs == null) {
+            return;
+        }
+
+        for (MavenLibrary lib : libs.value()) {
+            load(lib.groupId(), lib.artifactId(), lib.version(), lib.repo().url());
+        }
+    }
+
+    public static void load(String groupId, String artifactId, String version) {
+        load(groupId, artifactId, version, "http://repo1.maven.org/maven2");
+    }
+
+    public static void load(String groupId, String artifactId, String version, String repoUrl) {
+        load(new Dependency(groupId, artifactId, version, repoUrl));
+    }
+
+    public static void load(Dependency d) {
+        Log.info(String.format("Loading dependency %s:%s:%s from %s", d.getGroupId(), d.getArtifactId(), d.getVersion(), d.getRepoUrl()));
+        String name = d.getArtifactId() + "-" + d.getVersion();
+
+        File saveLocation = new File(getLibFolder(), name + ".jar");
+        if (!saveLocation.exists()) {
+
+            try {
+                Log.info("Dependency '" + name + "' is not already in the libraries folder. Attempting to download...");
+                URL url = d.getUrl();
+
+                try (InputStream is = url.openStream()) {
+                    Files.copy(is, saveLocation.toPath());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Log.info("Dependency '" + name + "' successfully downloaded.");
+        }
+
+        if (!saveLocation.exists()) {
+            throw new RuntimeException("Unable to download dependency: " + d.toString());
+        }
+
+        URLClassLoader classLoader = (URLClassLoader) LoaderUtils.getPlugin().getClass().getClassLoader().getParent();
+        try {
+            ADD_URL_METHOD.invoke(classLoader, saveLocation.toURI().toURL());
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load dependency: " + saveLocation.toString(), e);
+        }
+
+        Log.info("Loaded dependency '" + name + "' successfully.");
+    }
+
+    private static File getLibFolder() {
+        File pluginDataFolder = LoaderUtils.getPlugin().getDataFolder();
+        File pluginsDir = pluginDataFolder.getParentFile();
+
+        File helperDir = new File(pluginsDir, "helper");
+        File libs = new File(helperDir, "libraries");
+        libs.mkdirs();
+        return libs;
+    }
+
+    public static final class Dependency {
+        private final String groupId;
+        private final String artifactId;
+        private final String version;
+        private final String repoUrl;
+
+        public Dependency(String groupId, String artifactId, String version, String repoUrl) {
+            this.groupId = Preconditions.checkNotNull(groupId, "groupId");
+            this.artifactId = Preconditions.checkNotNull(artifactId, "artifactId");
+            this.version = Preconditions.checkNotNull(version, "version");
+            this.repoUrl = Preconditions.checkNotNull(repoUrl, "repoUrl");
+        }
+
+        public String getGroupId() {
+            return groupId;
+        }
+
+        public String getArtifactId() {
+            return artifactId;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public String getRepoUrl() {
+            return repoUrl;
+        }
+
+        public URL getUrl() throws MalformedURLException {
+            String repo = repoUrl;
+            if (!repo.endsWith("/")) {
+                repo += "/";
+            }
+            repo += "%s/%s/%s/%s-%s.jar";
+
+            String url = String.format(repo, groupId.replace(".", "/"), artifactId, version, artifactId, version);
+            return new URL(url);
+        }
+
+        public boolean equals(Object o) {
+            if (o == this) return true;
+            if (!(o instanceof Dependency)) return false;
+            final Dependency other = (Dependency) o;
+            return this.getGroupId().equals(other.getGroupId()) &&
+                    this.getArtifactId().equals(other.getArtifactId()) &&
+                    this.getVersion().equals(other.getVersion()) &&
+                    this.getRepoUrl().equals(other.getRepoUrl());
+        }
+
+        public int hashCode() {
+            final int PRIME = 59;
+            int result = 1;
+            result = result * PRIME + this.getGroupId().hashCode();
+            result = result * PRIME + this.getArtifactId().hashCode();
+            result = result * PRIME + this.getVersion().hashCode();
+            result = result * PRIME + this.getRepoUrl().hashCode();
+            return result;
+        }
+
+        public String toString() {
+            return "LibraryLoader.Dependency(" +
+                    "groupId=" + this.getGroupId() + ", " +
+                    "artifactId=" + this.getArtifactId() + ", " +
+                    "version=" + this.getVersion() + ", " +
+                    "repoUrl=" + this.getRepoUrl() + ")";
+        }
+    }
+
+
+}
---
+++ helper/src/main/java/me/lucko/helper/maven/MavenLibrary.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.maven;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to indicate a required library for a class.
+ */
+@Documented
+@Repeatable(MavenLibraries.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MavenLibrary {
+
+    /**
+     * The group id of the library
+     *
+     * @return the group id of the library
+     */
+    String groupId();
+
+    /**
+     * The artifact id of the library
+     *
+     * @return the artifact id of the library
+     */
+    String artifactId();
+
+    /**
+     * The version of the library
+     *
+     * @return the version of the library
+     */
+    String version();
+
+    /**
+     * The repo where the library can be obtained from
+     *
+     * @return the repo where the library can be obtained from
+     */
+    Repository repo() default @Repository(url = "http://repo1.maven.org/maven2");
+
+}
