fc57234a1f70bef82d545ba4a218346a24199945
Luck <git@lucko.me>
2017-08-29 16:20:13 +0100

    add helper-lilypad

---
+++ helper-lilypad/src/main/java/me/lucko/helper/lilypad/plugin/LilyPadWrapper.java
@@ -0,0 +1,126 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.lilypad.plugin;
+
+import com.google.common.reflect.TypeToken;
+
+import me.lucko.helper.lilypad.HelperLilyPad;
+import me.lucko.helper.messaging.AbstractMessenger;
+import me.lucko.helper.messaging.Channel;
+
+import lilypad.client.connect.api.Connect;
+import lilypad.client.connect.api.event.EventListener;
+import lilypad.client.connect.api.event.MessageEvent;
+import lilypad.client.connect.api.request.RequestException;
+import lilypad.client.connect.api.request.impl.MessageRequest;
+import lilypad.client.connect.api.request.impl.RedirectRequest;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.annotation.Nonnull;
+
+public class LilyPadWrapper implements HelperLilyPad {
+
+    private final Connect connect;
+    private final AbstractMessenger messenger;
+    private final AtomicBoolean listening = new AtomicBoolean(false);
+
+    public LilyPadWrapper(@Nonnull Connect connect) {
+        this.connect = connect;
+
+        messenger = new AbstractMessenger(
+                (channel, message) -> {
+                    try {
+                        connect.request(new MessageRequest(Collections.emptyList(), channel, message));
+                    } catch (RequestException | UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                },
+                channel -> {
+                    if (listening.getAndSet(true)) {
+                        return;
+                    }
+                    try {
+                        connect.registerEvents(this);
+                    } catch (Exception e) {
+                        listening.set(false);
+                    }
+                },
+                channel -> {}
+        );
+    }
+
+    @EventListener
+    public void onMessage(MessageEvent event) {
+        String channel = event.getChannel();
+        String message;
+        try {
+            message = event.getMessageAsString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        messenger.registerIncomingMessage(channel, message);
+    }
+
+    @Override
+    public void redirectPlayer(@Nonnull String serverId, @Nonnull String playerUsername) {
+        try {
+            connect.request(new RedirectRequest(serverId, playerUsername));
+        } catch (RequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Nonnull
+    @Override
+    public Connect getLilyPadConnect() {
+        return connect;
+    }
+
+    @Nonnull
+    @Override
+    public String getId() {
+        return connect.getSettings().getUsername();
+    }
+
+    @Nonnull
+    @Override
+    public Set<String> getGroups() {
+        return Collections.emptySet();
+    }
+
+    @Nonnull
+    @Override
+    public <T> Channel<T> getChannel(@Nonnull String name, @Nonnull TypeToken<T> type) {
+        return messenger.getChannel(name, type);
+    }
+
+}
