6e8e91281607c0ebc738a11436e4accc3fa95815
Luck <git@lucko.me>
2017-11-20 22:15:14 +0000

    init helper-profiles

---
+++ helper/src/main/java/me/lucko/helper/profiles/SimpleProfile.java
@@ -0,0 +1,83 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.profiles;
+
+import com.google.common.base.Preconditions;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+final class SimpleProfile implements Profile {
+
+    @Nonnull
+    private final UUID uniqueId;
+
+    @Nullable
+    private final String name;
+
+    SimpleProfile(@Nonnull UUID uniqueId, @Nullable String name) {
+        this.uniqueId = Preconditions.checkNotNull(uniqueId, "uniqueId");
+        this.name = name;
+    }
+
+    @Nonnull
+    @Override
+    public UUID getUniqueId() {
+        return uniqueId;
+    }
+
+    @Nonnull
+    @Override
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
+    }
+
+    @Override
+    public long getTimestamp() {
+        return -1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Profile)) return false;
+        final Profile other = (Profile) o;
+        return this.getUniqueId().equals(other.getUniqueId()) && this.getName().equals(other.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.uniqueId, this.name);
+    }
+
+    public String toString() {
+        return "Profile(uniqueId=" + this.uniqueId + ", name=" + this.name + ")";
+    }
+}
