bee384d143868cdca17aa270a4da7f03543ac77f
Luck <git@lucko.me>
2018-01-28 13:58:33 +0000

    fix compat with the previously used Base64 encoding

---
+++ helper/src/main/java/me/lucko/helper/serialize/Base64Util.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.serialize;
+
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.util.Base64;
+
+class Base64Util {
+    
+    public static String encode(byte[] buf) {
+        return Base64.getEncoder().encodeToString(buf);
+    }
+
+    public static byte[] decode(String src) {
+        try {
+            return Base64.getDecoder().decode(src);
+        } catch (IllegalArgumentException e) {
+            // compat with the previously used base64 encoder
+            try {
+                return Base64Coder.decodeLines(src);
+            } catch (Exception ignored) {
+                throw e;
+            }
+        }
+    }
+
+    private Base64Util() {}
+
+}
