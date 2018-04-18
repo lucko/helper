37c464726877805bd3b58b7f7f497b490f7baa95
Luck <git@lucko.me>
2018-04-18 22:03:10 +0100

    Implement lilypad redirect system

---
+++ helper-lilypad/src/main/java/me/lucko/helper/lilypad/redirect/RedirectSystem.java
@@ -0,0 +1,237 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.lilypad.redirect;
+
+import com.google.common.collect.ImmutableMap;
+
+import me.lucko.helper.lilypad.HelperLilyPad;
+import me.lucko.helper.profiles.Profile;
+import me.lucko.helper.promise.Promise;
+import me.lucko.helper.terminable.Terminable;
+
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Implements a system for controlled redirects between servers.
+ */
+public interface RedirectSystem extends Terminable {
+
+    /**
+     * Creates a new redirect system.
+     *
+     * <p>Only one system should be created per instance.</p>
+     *
+     * @param lilyPad the lilypad instance.
+     * @return the new system
+     */
+    static RedirectSystem create(HelperLilyPad lilyPad) {
+        return new RedirectSystemImpl(lilyPad);
+    }
+
+    /**
+     * Makes a request to redirect the given player to the given server.
+     *
+     * @param serverId the server to redirect to
+     * @param profile the player to be redirected
+     * @param params the parameters for the request
+     * @return a promise for the redirect response
+     */
+    Promise<ReceivedResponse> redirectPlayer(@Nonnull String serverId, @Nonnull Profile profile, @Nonnull Map<String, String> params);
+
+    /**
+     * Makes a request to redirect the given player to the given server.
+     *
+     * @param serverId the server to redirect to
+     * @param player the player to be redirected
+     * @param params the parameters for the request
+     * @return a promise for the redirect response
+     */
+    default Promise<ReceivedResponse> redirectPlayer(@Nonnull String serverId, @Nonnull Player player, @Nonnull Map<String, String> params) {
+        return redirectPlayer(serverId, Profile.create(player), params);
+    }
+
+    /**
+     * Sets the {@link RequestHandler} for this instance.
+     *
+     * @param handler the handler
+     */
+    void setHandler(@Nonnull RequestHandler handler);
+
+    /**
+     * Sets if the system should ensure that incoming connections were made (and accepted) by the
+     * redirect system.
+     *
+     * @param ensureJoinedViaQueue if the system should ensure all joins are in the redirect queue
+     */
+    void setEnsure(boolean ensureJoinedViaQueue);
+
+    /**
+     * Gets the number of connections which have been allowed, but not yet fully established.
+     *
+     * @return the number of expected connections
+     */
+    int getExpectedConnectionsCount();
+
+    @Override
+    void close();
+
+    /**
+     * Encapsulates a redirect request
+     */
+    interface Request {
+
+        /**
+         * Gets the profile sending the request
+         *
+         * @return the profile sending the request
+         */
+        @Nonnull
+        Profile getProfile();
+
+        /**
+         * Gets the parameters included with the request.
+         *
+         * @return the parameters
+         */
+        @Nonnull
+        Map<String, String> getParams();
+
+    }
+
+    /**
+     * Encapsulates the response to a {@link Request}.
+     */
+    final class Response {
+        private static final Response ALLOW = new Response(true, null, ImmutableMap.of());
+        private static final Response DENY = new Response(false, null, ImmutableMap.of());
+
+        public static Response allow() {
+            return ALLOW;
+        }
+
+        public static Response allow(@Nonnull Map<String, String> params) {
+            return new Response(true, null, params);
+        }
+
+        public static Response deny() {
+            return DENY;
+        }
+
+        public static Response deny(@Nonnull String reason) {
+            return new Response(false, reason, ImmutableMap.of());
+        }
+
+        public static Response deny(@Nonnull Map<String, String> params) {
+            return new Response(false, null, params);
+        }
+
+        public static Response deny(@Nonnull String reason, @Nonnull Map<String, String> params) {
+            return new Response(false, reason, params);
+        }
+
+        private final boolean allowed;
+        private final String reason;
+        private final Map<String, String> params;
+
+        public Response(boolean allowed, @Nullable String reason, @Nonnull Map<String, String> params) {
+            this.allowed = allowed;
+            this.reason = reason;
+            this.params = ImmutableMap.copyOf(params);
+        }
+
+        public boolean isAllowed() {
+            return this.allowed;
+        }
+
+        @Nullable
+        public String getReason() {
+            return this.reason;
+        }
+
+        public Map<String, String> getParams() {
+            return this.params;
+        }
+    }
+
+    /**
+     * Handles incoming redirect requests for this server
+     */
+    interface RequestHandler {
+
+        /**
+         * Handles the request and produces a result.
+         *
+         * @param request the request
+         * @return the response
+         */
+        @Nonnull
+        Promise<Response> handle(@Nonnull Request request);
+
+    }
+
+    /**
+     * Represents the response to a redirect request.
+     */
+    interface ReceivedResponse {
+
+        /**
+         * Gets the status of the response
+         *
+         * @return the status
+         */
+        @Nonnull
+        Status getStatus();
+
+        /**
+         * Gets the reason for the response.
+         *
+         * @return the reason
+         */
+        @Nonnull
+        Optional<String> getReason();
+
+        /**
+         * Gets the parameters included with the response.
+         *
+         * @return the parameters
+         */
+        @Nonnull
+        Map<String, String> getParams();
+
+        enum Status {
+            ALLOWED,
+            DENIED,
+            NO_REPLY
+        }
+    }
+
+}
---
+++ helper-lilypad/src/main/java/me/lucko/helper/lilypad/redirect/RedirectSystemImpl.java
@@ -0,0 +1,257 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.lilypad.redirect;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableMap;
+
+import me.lucko.helper.Events;
+import me.lucko.helper.event.SingleSubscription;
+import me.lucko.helper.lilypad.HelperLilyPad;
+import me.lucko.helper.messaging.conversation.ConversationChannel;
+import me.lucko.helper.messaging.conversation.ConversationChannelAgent;
+import me.lucko.helper.messaging.conversation.ConversationMessage;
+import me.lucko.helper.messaging.conversation.ConversationReply;
+import me.lucko.helper.messaging.conversation.ConversationReplyListener;
+import me.lucko.helper.profiles.Profile;
+import me.lucko.helper.promise.Promise;
+import me.lucko.helper.text.Text;
+
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+
+class RedirectSystemImpl implements RedirectSystem {
+    private final HelperLilyPad lilyPad;
+
+    private final ConversationChannel<RequestMessage, ResponseMessage> channel;
+    private final ConversationChannelAgent<RequestMessage, ResponseMessage> agent;
+
+    private final Cache<UUID, Response> expectedPlayers = CacheBuilder.newBuilder()
+            .expireAfterWrite(5, TimeUnit.SECONDS)
+            .build();
+
+    private final SingleSubscription<AsyncPlayerPreLoginEvent> loginEventListener;
+    private boolean ensureJoinedViaQueue = true;
+
+    private RequestHandler handler = new AllowAllHandler();
+
+    RedirectSystemImpl(HelperLilyPad lilyPad) {
+        this.lilyPad = lilyPad;
+
+        this.channel = this.lilyPad.getConversationChannel("hlp-redirect", RequestMessage.class, ResponseMessage.class);
+
+        this.agent = this.channel.newAgent();
+        this.agent.addListener((agent, message) -> {
+            if (!this.lilyPad.getId().equalsIgnoreCase(message.targetServer)) {
+                return ConversationReply.noReply();
+            }
+
+            // call the handler
+            Promise<Response> response = this.handler.handle(message);
+
+            // process the redirect
+            response.thenAcceptAsync(r -> {
+                if (!r.isAllowed()) {
+                    return;
+                }
+
+                // add player to the expected players queue
+                this.expectedPlayers.put(message.uuid, r);
+
+                // tell the connect server to move the player
+                this.lilyPad.redirectPlayer(this.lilyPad.getId(), message.username);
+            });
+
+            // send the response
+            return ConversationReply.ofPromise(response.thenApplyAsync(r -> {
+                ResponseMessage resp = new ResponseMessage();
+                resp.convoId = message.convoId;
+                resp.allowed = r.isAllowed();
+                resp.reason = r.getReason();
+                resp.params = new HashMap<>(r.getParams());
+                return resp;
+            }));
+        });
+
+        this.loginEventListener = Events.subscribe(AsyncPlayerPreLoginEvent.class)
+                .filter(e -> this.ensureJoinedViaQueue)
+                .handler(e -> {
+                    Response response = this.expectedPlayers.asMap().remove(e.getUniqueId());
+                    if (response == null || !response.isAllowed()) {
+                        e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, Text.colorize("&cSorry! The server is unable to process your login at this time. (queue error)"));
+                    }
+                });
+    }
+
+    @Override
+    public Promise<ReceivedResponse> redirectPlayer(@Nonnull String serverId, @Nonnull Profile profile, @Nonnull Map<String, String> params) {
+        RequestMessage req = new RequestMessage();
+        req.convoId = UUID.randomUUID();
+        req.targetServer = serverId;
+        req.uuid = profile.getUniqueId();
+        req.username = profile.getName().orElseThrow(() -> new IllegalArgumentException("Profile missing username"));
+        req.params = new HashMap<>(params);
+
+        Promise<ReceivedResponse> promise = Promise.empty();
+
+        // send req and await reply.
+        this.channel.sendMessage(req, new ConversationReplyListener<ResponseMessage>() {
+            @Nonnull
+            @Override
+            public RegistrationAction onReply(@Nonnull ResponseMessage reply) {
+                promise.supply(reply);
+                return RegistrationAction.STOP_LISTENING;
+            }
+
+            @Override
+            public void onTimeout(@Nonnull List<ResponseMessage> replies) {
+                promise.supply(MissingResponse.INSTANCE);
+            }
+        }, 5, TimeUnit.SECONDS);
+
+        return promise;
+    }
+
+    @Override
+    public void setHandler(@Nonnull RequestHandler handler) {
+        this.handler = Objects.requireNonNull(handler, "handler");
+    }
+
+    @Override
+    public void setEnsure(boolean ensureJoinedViaQueue) {
+        this.ensureJoinedViaQueue = ensureJoinedViaQueue;
+    }
+
+    @Override
+    public int getExpectedConnectionsCount() {
+        return (int) this.expectedPlayers.size();
+    }
+
+    @Override
+    public void close() {
+        this.agent.close();
+        this.loginEventListener.close();
+    }
+
+    private static final class RequestMessage implements ConversationMessage, Request {
+        private UUID convoId;
+        private String targetServer;
+        private UUID uuid;
+        private String username;
+        private Map<String, String> params;
+
+        @Nonnull
+        @Override
+        public UUID getConversationId() {
+            return this.convoId;
+        }
+
+        @Nonnull
+        @Override
+        public Profile getProfile() {
+            return Profile.create(this.uuid, this.username);
+        }
+
+        @Nonnull
+        @Override
+        public Map<String, String> getParams() {
+            return ImmutableMap.copyOf(this.params);
+        }
+    }
+
+    private static final class ResponseMessage implements ConversationMessage, ReceivedResponse {
+        private UUID convoId;
+        private boolean allowed;
+        private String reason;
+        private Map<String, String> params;
+
+        @Nonnull
+        @Override
+        public UUID getConversationId() {
+            return this.convoId;
+        }
+
+        @Nonnull
+        @Override
+        public Status getStatus() {
+            return this.allowed ? Status.ALLOWED : Status.DENIED;
+        }
+
+        @Nonnull
+        @Override
+        public Optional<String> getReason() {
+            return Optional.ofNullable(this.reason);
+        }
+
+        @Nonnull
+        @Override
+        public Map<String, String> getParams() {
+            return ImmutableMap.copyOf(this.params);
+        }
+    }
+
+    private static final class MissingResponse implements ReceivedResponse {
+        private static final MissingResponse INSTANCE = new MissingResponse();
+
+        @Nonnull
+        @Override
+        public Status getStatus() {
+            return Status.NO_REPLY;
+        }
+
+        @Nonnull
+        @Override
+        public Optional<String> getReason() {
+            return Optional.empty();
+        }
+
+        @Nonnull
+        @Override
+        public Map<String, String> getParams() {
+            return ImmutableMap.of();
+        }
+    }
+
+    private static final class AllowAllHandler implements RequestHandler {
+
+        @Nonnull
+        @Override
+        public Promise<Response> handle(@Nonnull Request request) {
+            return Promise.completed(Response.allow());
+        }
+    }
+
+}
