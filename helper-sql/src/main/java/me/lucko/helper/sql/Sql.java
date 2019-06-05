7c500785dca89fa78bba47d7b836c48defefbeda
FerusGrim <awildferus@gmail.com>
2019-06-05 15:10:35 -0400

    HelperSql: Convenience methods, config updates.

--- helper-sql/src/main/java/me/lucko/helper/sql/Sql.java
+++ helper-sql/src/main/java/me/lucko/helper/sql/Sql.java
@@ -27,10 +27,17 @@ package me.lucko.helper.sql;
 
 import com.zaxxer.hikari.HikariDataSource;
 
+import me.lucko.helper.Schedulers;
+import me.lucko.helper.promise.Promise;
+import me.lucko.helper.sql.util.ThrownConsumer;
+import me.lucko.helper.sql.util.ThrownFunction;
 import me.lucko.helper.terminable.Terminable;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 import javax.annotation.Nonnull;
 
@@ -38,6 +45,8 @@ import javax.annotation.Nonnull;
  * Represents an individual SQL datasource, created by the library.
  */
 public interface Sql extends Terminable {
+
+    ThrownConsumer<PreparedStatement, SQLException> NOTHING = ThrownConsumer.nothing();
 
     /**
      * Gets the Hikari instance backing the datasource
@@ -57,4 +66,179 @@ public interface Sql extends Terminable {
     @Nonnull
     Connection getConnection() throws SQLException;
 
+    /**
+     * Executes a database statement with no preparation.
+     *
+     * <p>This will be executed on an asynchronous thread.</p>
+     *
+     * @param statement the statement to be executed
+     * @return a Promise of an asynchronous database execution
+     * @see #execute(String) to perform this action synchronously
+     */
+    @Nonnull
+    default Promise<Void> executeAsync(@Nonnull String statement) {
+        return Schedulers.async().run(() -> this.execute(statement));
+    }
+
+    /**
+     * Executes a database statement with no preparation.
+     *
+     * <p>This will be executed on whichever thread it's called from.</p>
+     *
+     * @param statement the statement to be executed
+     * @see #executeAsync(String) to perform the same action asynchronously
+     */
+    default void execute(@Nonnull String statement) {
+        this.execute(statement, NOTHING);
+    }
+
+    /**
+     * Executes a database statement with preparation.
+     *
+     * <p>This will be executed on an asynchronous thread.</p>
+     *
+     * @param statement the statement to be executed
+     * @param preparer the preparation used for this statement
+     * @return a Promise of an asynchronous database execution
+     * @see #executeAsync(String, ThrownConsumer) to perform this action synchronously
+     */
+    @Nonnull
+    default Promise<Void> executeAsync(@Nonnull String statement,
+                               @Nonnull ThrownConsumer<PreparedStatement, SQLException> preparer) {
+        return Schedulers.async().run(() -> this.execute(statement, preparer));
+    }
+
+    /**
+     * Executes a database statement with preparation.
+     *
+     * <p>This will be executed on whichever thread it's called from.</p>
+     *
+     * @param statement the statement to be executed
+     * @param preparer the preparation used for this statement
+     * @see #executeAsync(String, ThrownConsumer) to perform this action asynchronously
+     */
+    void execute(@Nonnull String statement,
+                 @Nonnull ThrownConsumer<PreparedStatement, SQLException> preparer);
+
+    /**
+     * Executes a database query with no preparation.
+     *
+     * <p>This will be executed on an asynchronous thread.</p>
+     *
+     * <p>In the case of a {@link SQLException} or in the case of
+     * no data being returned, or the handler evaluating to null,
+     * this method will return an {@link Optional#empty()} object.</p>
+     *
+     * @param query the query to be executed
+     * @param handler the handler for the data returned by the query
+     * @param <R> the returned type
+     * @return a Promise of an asynchronous database query
+     * @see #query(String, ThrownFunction) to perform this query synchronously
+     */
+    default <R> Promise<Optional<R>> queryAsync(@Nonnull String query,
+                                        @Nonnull ThrownFunction<ResultSet, R, SQLException> handler) {
+        return Schedulers.async().supply(() -> this.query(query, handler));
+    }
+
+    /**
+     * Executes a database query with no preparation.
+     *
+     * <p>This will be executed on whichever thread it's called from.</p>
+     *
+     * <p>In the case of a {@link SQLException} or in the case of
+     * no data being returned, or the handler evaluating to null,
+     * this method will return an {@link Optional#empty()} object.</p>
+     *
+     * @param query the query to be executed
+     * @param handler the handler for the data returned by the query
+     * @param <R> the returned type
+     * @return the results of the database query
+     * @see #queryAsync(String, ThrownFunction) to perform this query asynchronously
+     */
+    default <R> Optional<R> query(@Nonnull String query,
+                          @Nonnull ThrownFunction<ResultSet, R, SQLException> handler) {
+        return this.query(query, NOTHING, handler);
+    }
+
+    /**
+     * Executes a database query with preparation.
+     *
+     * <p>This will be executed on an asynchronous thread.</p>
+     *
+     * <p>In the case of a {@link SQLException} or in the case of
+     * no data being returned, or the handler evaluating to null,
+     * this method will return an {@link Optional#empty()} object.</p>
+     *
+     * @param query the query to be executed
+     * @param preparer the preparation used for this statement
+     * @param handler the handler for the data returned by the query
+     * @param <R> the returned type
+     * @return a Promise of an asynchronous database query
+     * @see #query(String, ThrownFunction) to perform this query synchronously
+     */
+    default <R> Promise<Optional<R>> queryAsync(@Nonnull String query,
+                                        @Nonnull ThrownConsumer<PreparedStatement, SQLException> preparer,
+                                        @Nonnull ThrownFunction<ResultSet, R, SQLException> handler) {
+        return Schedulers.async().supply(() -> this.query(query, preparer, handler));
+    }
+    /**
+     * Executes a database query with preparation.
+     *
+     * <p>This will be executed on whichever thread it's called from.</p>
+     *
+     * <p>In the case of a {@link SQLException} or in the case of
+     * no data being returned, or the handler evaluating to null,
+     * this method will return an {@link Optional#empty()} object.</p>
+     *
+     * @param query the query to be executed
+     * @param preparer the preparation used for this statement
+     * @param handler the handler for the data returned by the query
+     * @param <R> the returned type
+     * @return the results of the database query
+     * @see #queryAsync(String, ThrownFunction) to perform this query asynchronously
+     */
+    <R> Optional<R> query(@Nonnull String query,
+                          @Nonnull ThrownConsumer<PreparedStatement, SQLException> preparer,
+                          @Nonnull ThrownFunction<ResultSet, R, SQLException> handler);
+
+    /**
+     * Executes a batched database execution.
+     *
+     * <p>This will be executed on an asynchronous thread.</p>
+     *
+     * <p>Note that proper implementations of this method should determine
+     * if the provided {@link BatchBuilder} is actually worth of being a
+     * batched statement. For instance, a BatchBuilder with only one
+     * handler can safely be referred to {@link #executeAsync(String, ThrownConsumer)}</p>
+     *
+     * @param builder the builder to be used.
+     * @return a Promise of an asynchronous batched database execution
+     * @see #executeBatch(BatchBuilder) to perform this action synchronously
+     */
+    default Promise<Void> executeBatchAsync(@Nonnull BatchBuilder builder) {
+        return Schedulers.async().run(() -> this.executeBatch(builder));
+    }
+
+    /**
+     * Executes a batched database execution.
+     *
+     * <p>This will be executed on whichever thread it's called from.</p>
+     *
+     * <p>Note that proper implementations of this method should determine
+     * if the provided {@link BatchBuilder} is actually worth of being a
+     * batched statement. For instance, a BatchBuilder with only one
+     * handler can safely be referred to {@link #execute(String, ThrownConsumer)}</p>
+     *
+     * @param builder the builder to be used.
+     * @see #executeBatchAsync(BatchBuilder) to perform this action asynchronously
+     */
+    void executeBatch(@Nonnull BatchBuilder builder);
+
+    /**
+     * Gets a {@link BatchBuilder} for the provided statement.
+     *
+     * @param statement the statement to prepare for batching.
+     * @return a BatchBuilder
+     */
+    BatchBuilder batch(@Nonnull String statement);
 }
