654110aef7db6463b284c01802233176d196513e
Luck <git@lucko.me>
2019-08-09 21:24:34 +0100

    Switch from fixed size to cached thread pool for helper's built-in async executor

---
+++ helper/src/main/java/me/lucko/helper/scheduler/HelperAsyncExecutor.java
@@ -0,0 +1,135 @@
+/*
+ * This file is part of helper, licensed under the MIT License.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package me.lucko.helper.scheduler;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.WeakHashMap;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+final class HelperAsyncExecutor extends AbstractExecutorService implements ScheduledExecutorService {
+    private final ExecutorService taskService;
+    private final ScheduledExecutorService timerExecutionService;
+
+    private final Set<ScheduledFuture<?>> tasks = Collections.newSetFromMap(new WeakHashMap<>());
+
+    HelperAsyncExecutor() {
+        this.taskService = Executors.newCachedThreadPool(new ThreadFactoryBuilder()
+                .setDaemon(true)
+                .setNameFormat("helper-scheduler-%d")
+                .build()
+        );
+        this.timerExecutionService = Executors.newSingleThreadScheduledExecutor(new ThreadFactoryBuilder()
+                .setDaemon(true)
+                .setNameFormat("helper-scheduler-timer")
+                .build()
+        );
+    }
+
+    private ScheduledFuture<?> consumeTask(ScheduledFuture<?> future) {
+        this.tasks.add(future);
+        return future;
+    }
+
+    public void cancelRepeatingTasks() {
+        for (ScheduledFuture<?> task : this.tasks) {
+            task.cancel(false);
+        }
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+        this.taskService.execute(HelperExecutors.wrapRunnable(runnable));
+    }
+
+    @Override
+    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+        return consumeTask(this.timerExecutionService.schedule(new Worker(HelperExecutors.wrapRunnable(command)), delay, unit));
+    }
+
+    @Override
+    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+        return consumeTask(this.timerExecutionService.scheduleAtFixedRate(new Worker(HelperExecutors.wrapRunnable(command)), initialDelay, period, unit));
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+        return consumeTask(this.timerExecutionService.scheduleWithFixedDelay(new Worker(HelperExecutors.wrapRunnable(command)), initialDelay, delay, unit));
+    }
+
+    @Override
+    public void shutdown() {
+        // noop
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        // noop
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) {
+        throw new IllegalStateException("Not shutdown");
+    }
+
+    private final class Worker implements Runnable {
+        private final Runnable delegate;
+
+        private Worker(Runnable delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public void run() {
+            HelperAsyncExecutor.this.taskService.execute(delegate);
+        }
+    }
+}
