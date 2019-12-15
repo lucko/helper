1c8cf5d1c9d7c107655769d3629cc62601d728e4
FerusGrim <awildferus@gmail.com>
2019-12-15 14:17:38 -0500

    Add support for 1.15

--- helper/src/main/java/me/lucko/helper/reflect/MinecraftVersions.java
+++ helper/src/main/java/me/lucko/helper/reflect/MinecraftVersions.java
@@ -38,6 +38,16 @@ import java.util.regex.Pattern;
 public final class MinecraftVersions {
 
     /**
+     * Version 1.15 - buzzy bees update
+     */
+    public static final MinecraftVersion v1_15 = MinecraftVersion.parse("1.15");
+
+    /**
+     * Version 1.14 - village and pillage update
+     */
+    public static final MinecraftVersion v1_14 = MinecraftVersion.parse("1.14");
+
+    /**
      * Version 1.13 - update aquatic.
      */
     public static final MinecraftVersion v1_13 = MinecraftVersion.parse("1.13");
