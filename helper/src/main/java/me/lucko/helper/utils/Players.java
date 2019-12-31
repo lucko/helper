12994d873d164c68e13f96f5ee86691fe08e86a6
WI5E <49742501+WI5E@users.noreply.github.com>
2019-12-31 15:26:31 -0800

    Update Players.java
    Remove code redundancy

--- helper/src/main/java/me/lucko/helper/utils/Players.java
+++ helper/src/main/java/me/lucko/helper/utils/Players.java
@@ -164,9 +164,7 @@ public final class Players {
      * @param consumer the action to apply
      */
     public static void forEachInRange(Location center, double radius, Consumer<Player> consumer) {
-        center.getWorld().getNearbyEntities(center, radius, radius, radius).stream()
-                .filter(e -> e instanceof Player)
-                .map(e -> ((Player) e))
+        streamInRange(center, radius)
                 .forEach(consumer);
     }
 
