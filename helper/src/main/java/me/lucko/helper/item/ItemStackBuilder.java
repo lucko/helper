b01c4674904c609dcd4ce84e25ea97659152f378
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2020-05-18 23:36:05 +0200

    spacing

--- helper/src/main/java/me/lucko/helper/item/ItemStackBuilder.java
+++ helper/src/main/java/me/lucko/helper/item/ItemStackBuilder.java
@@ -178,7 +178,7 @@ public final class ItemStackBuilder {
             if (type == Material.LEATHER_BOOTS || type == Material.LEATHER_CHESTPLATE || type == Material.LEATHER_HELMET || type == Material.LEATHER_LEGGINGS) {
                 LeatherArmorMeta meta = (LeatherArmorMeta) itemStack.getItemMeta();
 
-                if (meta!= null) {
+                if (meta != null) {
                     meta.setColor(color);
                     itemStack.setItemMeta(meta);
                 }
