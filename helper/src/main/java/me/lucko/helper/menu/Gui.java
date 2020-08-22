d684db829a3d7c30b9886a83749b179348e3bea3
Callum <38501234+calumari@users.noreply.github.com>
2020-08-22 09:36:45 +0100

    Update to 1.16.x (#83)
    * Fix PlayerInteractEvent firing on inventory click
    * Translate BungeeCord format RGB colors
    * Update newest Minecraft version
    * Make Gui open delay 1.16+ exclusive
    * Fix typo

--- helper/src/main/java/me/lucko/helper/menu/Gui.java
+++ helper/src/main/java/me/lucko/helper/menu/Gui.java
@@ -31,6 +31,8 @@ import me.lucko.helper.Schedulers;
 import me.lucko.helper.metadata.Metadata;
 import me.lucko.helper.metadata.MetadataKey;
 import me.lucko.helper.metadata.MetadataMap;
+import me.lucko.helper.reflect.MinecraftVersion;
+import me.lucko.helper.reflect.MinecraftVersions;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.composite.CompositeTerminable;
 import me.lucko.helper.text.Text;
@@ -256,10 +258,23 @@ public abstract class Gui implements TerminableConsumer {
     }
 
     public void open() {
+        if (MinecraftVersion.getRuntimeVersion().isAfterOrEq(MinecraftVersions.v1_16)) {
+            // delay by a tick in 1.16+ to prevent an unwanted PlayerInteractEvent interfering with inventory clicks
+            Schedulers.sync().runLater(() -> {
+                if (!this.player.isOnline()) {
+                    return;
+                }
+                handleOpen();
+            }, 1);
+        } else {
+            handleOpen();
+        }
+    }
+
+    private void handleOpen() {
         if (this.valid) {
             throw new IllegalStateException("Gui is already opened.");
         }
-
         this.firstDraw = true;
         this.invalidated = false;
         try {
--- helper/src/main/java/me/lucko/helper/text3/Text.java
+++ helper/src/main/java/me/lucko/helper/text3/Text.java
@@ -87,7 +87,7 @@ public final class Text {
     public static String translateAlternateColorCodes(char from, char to, String textToTranslate) {
         char[] b = textToTranslate.toCharArray();
         for (int i = 0; i < b.length - 1; i++) {
-            if (b[i] == from && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i+1]) > -1) {
+            if (b[i] == from && "0123456789AaBbCcDdEeFfKkLlMmNnOoRrXx".indexOf(b[i+1]) > -1) {
                 b[i] = to;
                 b[i+1] = Character.toLowerCase(b[i+1]);
             }
